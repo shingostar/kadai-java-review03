09c86e4fe37bdc738230f57ab22b06dfec776546
shingostar <shingostar@shore.ocn.ne.jp>
2023-12-24 23:28:48 +0900

    BaseBallTeam.java を更新

--- src/baseball/BaseBallTeam.java
+++ src/baseball/BaseBallTeam.java
@@ -1,22 +1,19 @@
-package baseball;
+package animal;
 
-public class BaseBallTeam {
+public class Animal{
 
     private String name;
-    private int win;
-    private int lose;
-    private int draw;
+    private int name;
+    private int age;
 
-    public BaseBallTeam(String name, int win, int lose, int draw) {
+    public Review04(String name, int age) {
         this.name = name;
-        this.win = win;
-        this.lose = lose;
-        this.draw = draw;
+        this.age = age;
     }
     public double getRate() {
         return(double) win/(win+lose);
     }
     public void report() {
-        System.out.println(this.name+"の2022年の成績は"+win+"勝"+lose+"敗"+draw+"分、勝率は "+getRate()+"です。");
+        System.out.println(this.name+"です。"+age+"歳です。");
     }
 }
